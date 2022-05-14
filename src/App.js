25b131cd7ea9f15a02610d0b978a2ae684c1cff9
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-05-13 21:55:17 -0500

    added theo's letter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ImageOne from "./components/ImageOne";
 import ImageTwo from "./components/ImageTwo";
 import ImageThree from "./components/ImageThree";
 import TextBox from "./components/TextBox";
+import TextBox2 from "./components/TextBox2";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <ImageOne />
       <TextBox />
       <ImageTwo />
-      <TextBox />
+      <TextBox2 />
 
       <ImageThree />
       <TextBox />
---
+++ src/components/TextBox2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import theo from '../images/theo.jpg'
+
+const TextBox = () => {
+  return (
+    <div className='text-box'>
+        <img src={theo} />
+    </div>
+  )
+}
+
+export default TextBox
