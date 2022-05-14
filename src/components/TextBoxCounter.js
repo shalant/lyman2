fb3e72c844299b1652eb8d85967ccbfbeb74d811
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-05-14 11:54:19 -0500

    adding counter

---
+++ src/components/TextBoxCounter.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './TextBoxCounter.css';
+
+const TextBoxCounter = () => {
+  return (
+    <div className='text-box'>
+        <h3>Blah</h3>
+        <p>lorem ipsum</p>
+        <header>
+            <div className='container'>
+                <h1>Animated Counter</h1>
+                
+            </div>
+        </header>
+
+        <section className='counters'>
+            <div className='container'>
+                <div>
+                    <i className='fab fa-youtube fa-4x'></i>
+                    <div className='counter' data-target='38000'>0</div>
+                    <h3>Auto deaths this year</h3>
+                </div>
+                <div>
+                    <i className='fab fa-twitter fa-4x'></i>
+                    <div className='counter' data-target='15000'>0</div>
+                    <h3>Auto deaths this year</h3>
+                </div>
+                <div>
+                    <i className='fab fa-facebook fa-4x'></i>
+                    <div className='counter' data-target='9000'>0</div>
+                    <h3>Auto deaths this year</h3>
+                </div>
+                <div>
+                    <i className='fab fa-linkedin fa-4x'></i>
+                    <div className='counter' data-target='5000'>0</div>
+                    <h3>Auto deaths this year</h3>
+                </div>
+            </div>
+        </section>
+        
+        
+    </div>
+  )
+}
+
+export default TextBoxCounter;
