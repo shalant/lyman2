d8961e288e7fe964bbaaa38e7e823d82d56ff9e3
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-05-15 15:18:55 -0500

    replaced problematic picture

--- src/components/ImageTwo.js
+++ src/components/ImageTwo.js
@@ -1,5 +1,5 @@
 import { Parallax } from 'react-parallax';
-import kids2 from '../images/IMG_7338.jpg';
+import kids2 from '../images/lymanpic.jpg';
 
 const ImageTwo = () => (
     <Parallax className='image' bgImage={kids2} strength={800}>
